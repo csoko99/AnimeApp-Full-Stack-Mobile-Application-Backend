b6c04f94e1d82e7ff07b359d1848f71ad7c07cca
tanulo <tanulo@barossgabor.local>
2021-12-13 12:27:26 +0100

    kk

--- backend.js
+++ backend.js
@@ -36,6 +36,32 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/Action', (req, res) => {
+  
+
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'animedb'
+})
+
+connection.connect()
+    
+    connection.query('SELECT * FROM `animek` WHERE `anime_mufaj` LIKE "%Action%"', function (err, rows, fields) {
+       // if (err) throw err;
+      
+      console.log(rows)
+      res.send(rows)
+    })
+    
+    connection.end()
+ 
+ 
+
+  })
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
